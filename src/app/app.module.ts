2e66c86b82fd225c1bccff1f3c5c84f9b6481d91
Francois Oliveira <francois@objetorelacional.com.br>
2018-11-29 16:07:36 -0300

    Update project layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire';
 
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import {GMapModule} from 'primeng/gmap';
+
+import { GMapModule } from 'primeng/gmap';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +20,13 @@ import {GMapModule} from 'primeng/gmap';
   ],
   imports: [
     BrowserModule,
-    GMapModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    GMapModule,
+    TableModule,
+    MenubarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
